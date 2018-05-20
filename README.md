0212c4c850e1af75ab7edce0320514f0b21868f2
Grant Hanrahan <grantjhanrahan@gmail.com>
2018-05-20 21:01:56 +1000

    added html accessibility tags

--- README.md
+++ README.md
@@ -1,24 +1,38 @@
-## FinderAU Page Layout + Listing
-
-____
+# FinderAU Page Layout + Listing
 
 Link to live site: https://grantjhanrahan.github.io/fau-design/
 
-# Challenge
+
+## Challenge
 
 Reproduce a given layout using JavaScript and whatever front end technologies I felt were appropriate.
 
 The solution needed to consider the needs of a wide variety of users, fills in the interactions left out by the designer and appreciates that the template should be easy to update by other developers.
 
-# Approach
+
+## Technologies used
 
 - HTML
 - CSS
 - JavaScript
 - jQuery
+- AJAX
 
 
-# To do
+## Approach
+
+I began the project by building the basic structure of the HTML. From there I started to build on the CSS required. When implementing the CSS I tried to follow the FinderAU styling guide;
+- Responsive web design: https://github.com/finderau/styleguide/blob/master/pages/browser-support.md
+- Sass styleguide (even though I used CSS): https://github.com/finderau/styleguide/blob/master/pages/sass.md
+- BEM methodology for class and id names
+- JavaScript styleguide: https://github.com/finderau/styleguide/blob/master/pages/js.md
+
+I aimed to improve accessibility by incorporating WAI-ARIA Landmarks to help make it easier for people using screen readers to navigate through the site: https://developer.paciellogroup.com/blog/2013/02/using-wai-aria-landmarks-2013/
+
+The use of jQuery for the AJAX call wasn't required but I found that I preferred the formatting it provided compared to a XMLHttpRequest Object (https://www.w3schools.com/js/js_ajax_http.asp)
+
+
+## To do
 
 If I had to approach the challenge again;
 - I would instead use Sass styling to remove the duplicated css styling rules.
--- js/main.js
+++ js/main.js
@@ -1,39 +1,36 @@
-
-$(function(){
+$(function() {
 
   $.ajax({
 
-    url: "https://raw.githubusercontent.com/finderau/recruitment/master/front-end/design-and-js/01-page-layout-listing/data.json",
-    type: "GET",
-    dataType: "json",
+    url: "https://raw.githubusercontent.com/finderau/recruitment/master/front-end/design-and-js/01-page-layout-listing/data.json", type: "GET", dataType: "json",
 
     // Code to run if the request succeeds.
     // The response is passed to the 'success' function as the 'data' argument
-    success: function( data ) {
-      showArticles( data );
+    success: function(data) {
+      showArticles(data);
     },
 
     // Code to run if the request fails.
     // The request, the status code of the request and the error thrown are passed to the 'error' function as arguments
-    error: function( xhr, status, errorThrown ) {
-      alert( "Sorry, there was a problem!" );
-      console.log( "Error: " + errorThrown );
-      console.log( "Status: " + status );
-      console.dir( xhr );
+    error: function(xhr, status, errorThrown) {
+      alert("Sorry, there was a problem!");
+      console.log("Error: " + errorThrown);
+      console.log("Status: " + status);
+      console.dir(xhr);
     },
 
     // Code to run when the request is completed, regardless of success or failure
-    complete: function( xhr, status ) {
-      console.log( "The request is complete." );
+    complete: function(xhr, status) {
+      console.log("The request is complete.");
     }
 
   });
 
-  const showArticles = function( article ) {
+  const showArticles = function(article) {
     $articles = $("#content-posts");
 
     // Loop through the response data until the 5th article
-    for ( let i = 0; i < 5; i++ ) {
+    for (let i = 0; i < 5; i++) {
       post = article[i]
 
       let $articleDiv = $("<div></div>");
@@ -44,7 +41,6 @@ $(function(){
       $articleHeading.attr("href", `${post.url}`);
       // Append the hyperlinked article title to the $articleDiv
       $articleDiv.append($articleHeading)
-
 
       let $articleDescription = $("<p></p>")
       $articleDescription.text(`${post.description}`);
